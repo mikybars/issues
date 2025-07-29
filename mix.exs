6587c45130479393634e7277a5551baaf18e5d2b
mikybars <mperezibars@gmail.com>
2025-07-29 11:14:01 +0200

    Create project documentation

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/mikybars/issues",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,9 +25,9 @@ defmodule Issues.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.0.0"},
-      {:poison, "~> 3.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.19"},
+      {:earmark, "~> 1.2.4"}
     ]
   end
 
